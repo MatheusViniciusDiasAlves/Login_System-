84dfbfcf92447aec284ce7df14abaa7a926e35da
Matheus TI- Developer <178905543+MatheusViniciusDiasAlves@users.noreply.github.com>
2026-08-11 11:57:04 -0300

    Terminando de criar rotas de login no backend ao fronend

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,32 @@ const PORT = 3000;
 app.use(express.json());
 
 app.post("/login", (req, res) => {
-    console.log(req.body);
+
+    const { email, password } = req.body;
+
+    console.log("Login recebido:");
+    console.log("Email:", email);
+    console.log("Senha:", password);
 
     res.json({
-        mensagem: "Dados recebidos pelo backend!"
+        sucesso: true,
+        mensagem: "Login recebido pelo backend!"
     });
 });
+
+app.post("/cadastro", (req, res) =>{
+
+    const {nome, email, senha} = req.body;
+
+    console.log("Nome:", nome);
+    console.log("Email:", email);
+    console.log("Senha:", senha);
+
+    res.json({
+        sucesso: true,
+        mensagem: "Cadastro recebido pelo backend!"
+    });
+})
 
 app.listen(PORT, () =>{
     console.log(`Servidor funcinando na porta ${PORT}`)
--- frontend/login.js
+++ frontend/login.js
@@ -1,4 +1,4 @@
-const send = require("send");
+
 
 function meuEmail(){
 
@@ -19,6 +19,42 @@ function meuEmail(){
     })
     .then(resposta => resposta.json())
     .then(dados => {
-        console.log(dados)
+        console.log(dados);
+
+        if (dados.sucesso) {
+            alert(dados.mensagem);
+        } else {
+            alert(dados.mensagem);
+        }
+    })
+}
+
+
+function chamarCadastro(){
+    document.getElementById("cadastramentoDiv").style.display = "block";
+}
+
+function cadastrarDados(){
+
+    let nome  = document.getElementById("nomeCadastrado").value;
+    let email = document.getElementById("emailCadastro").value;
+    let senha = document.getElementById("senhaCadastrada").value;
+
+    fetch("http://localhost:3000/cadastro",{
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+             nome: nome,
+            email: email,
+            senha: senha
+        })
+    })
+    .then(resposta => resposta.json())
+    .then(dados => {
+        console.log(dados);
     })
 }
